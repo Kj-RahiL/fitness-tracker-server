af515b239df5ba30eada9ca15f0f375976626b1a
Kj-RahiL <rahiilarham@gmail.com>
2023-11-29 01:44:58 +0600

    post user and deleted betrainer

--- index.js
+++ index.js
@@ -37,99 +37,100 @@ async function run() {
         app.post('/jwt', async (req, res) => {
             const user = req.body;
             const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
-              expiresIn: "2h"
+                expiresIn: "2h"
             })
             res.send({ token })
-          })
+        })
 
-          const verifyToken = (req, res, next) => {
+        const verifyToken = (req, res, next) => {
             // console.log('inside verify token', req.headers.authorization)
             if (!req.headers.authorization) {
-              return res.status(401).send({ message: 'unauthorized access' })
+                return res.status(401).send({ message: 'unauthorized access' })
             }
             const token = req.headers.authorization.split(' ')[1];
             jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
-              if (err) {
-                return res.status(401).send({ message: "unauthorized access" })
-              }
-              req.decoded = decoded
-              next()
+                if (err) {
+                    return res.status(401).send({ message: "unauthorized access" })
+                }
+                req.decoded = decoded
+                next()
             })
-          }
-      
-          const verifyAdmin = async (req, res, next) => {
+        }
+
+        const verifyAdmin = async (req, res, next) => {
             const email = req.decoded.email;
             const query = { email: email };
             const user = await userCollection.findOne(query);
             const isAdmin = user?.role === 'admin';
             if (!isAdmin) {
-              return res.status(403).send({ message: 'forbidden access' });
+                return res.status(403).send({ message: 'forbidden access' });
             }
             next();
-          }
-      
+        }
+
         // users api
-        app.get('users', async(req,res)=>{
-            
+        app.get('/users', async (req, res) => {
+            const result = await userCollection.find().toArray()
+            res.send(result)
         })
-        app.get('/users/admin/:email', verifyToken,  async (req, res) => {
+        app.get('/users/admin/:email', verifyToken, async (req, res) => {
             const email = req.params.email
             // console.log(email)
             if (email !== req.decoded.email) {
-              return res.status(403).send({ message: 'unauthorized access' })
+                return res.status(403).send({ message: 'unauthorized access' })
             }
             const query = { email: email }
             const user = await userCollection.findOne(query)
             let admin = false
             if (user) {
-              admin = user?.role === 'admin'
+                admin = user?.role === 'admin'
             }
             res.send({ admin })
-          })
+        })
 
-        app.post('/users', async(req,res)=>{
+        app.post('/users', async (req, res) => {
             const user = req.body
-            const query = {email: user.email}
+            const query = { email: user.email }
             const existingUser = await userCollection.findOne(query)
-            if(existingUser){
-                return res.send({message: "user already exists", insertedId: null})
+            if (existingUser) {
+                return res.send({ message: "user already exists", insertedId: null })
             }
             const result = await userCollection.insertOne(user)
             res.send(result)
         })
 
         // newsletter api
-        app.get('/subscribe', async(req,res)=>{
+        app.get('/subscribe', async (req, res) => {
             const result = await subscribeCollection.find().toArray()
             res.send(result)
         })
-        app.post('/subscribe', async(req,res)=>{
+        app.post('/subscribe', async (req, res) => {
             const user = req.body
-            const query = {email: user.email}
+            const query = { email: user.email }
             const existingUser = await subscribeCollection.findOne(query)
             console.log(user, existingUser)
-            if(existingUser){
-                return res.send({message: "already subscribe", insertedId: null})
+            if (existingUser) {
+                return res.send({ message: "already subscribe", insertedId: null })
             }
             const result = await subscribeCollection.insertOne(user)
             res.send(result)
         })
 
         // trainer api
-        app.get('/trainer', async(req,res)=>{
+        app.get('/trainer', async (req, res) => {
             const result = await trainerCollection.find().toArray()
             res.send(result)
         })
-        app.get('/trainer/:id', async(req,res)=>{
+        app.get('/trainer/:id', async (req, res) => {
             const id = req.params.id
             const query = { _id: new ObjectId(id) }
             const result = await trainerCollection.findOne(query)
             res.send(result)
         })
-      
+
 
         //Be trainer related api
-        app.get('/beTrainer', async(req,res)=>{
+        app.get('/beTrainer', async (req, res) => {
             const result = await beTrainerCollection.find().toArray()
             res.send(result)
         })
@@ -137,7 +138,14 @@ async function run() {
             const trainer = req.body;
             const result = await beTrainerCollection.insertOne(trainer)
             res.send(result)
-          })
+        })
+
+        app.delete('/beTrainer/:id', async (req, res) => {
+            const id = req.params.id
+            const query = { _id: new ObjectId(id) }
+            const result = await beTrainerCollection.deleteOne(query)
+            res.send(result)
+        })
 
 
         // Send a ping to confirm a successful connection
